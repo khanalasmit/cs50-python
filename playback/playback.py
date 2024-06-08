7ac46ad89f057aa3774a7b4ede3a5d8e64036244
khanalasmit <asmitpasa2063@gmail.com>
2024-06-08 08:26:07 +0000

    Sat, Jun 8, 2024, 2:11 PM +05:45

--- playback/playback.py
+++ playback/playback.py
@@ -1,6 +1,8 @@
 def main():
     string=input()
-    convert(string)
+    next_string=convert(string)
+    print(next_string)
+
 def convert(new_string):
-    print(new_string.replace(":)","\N{slightly smiling face}").replace(":(","\N{slightly frowning face}"))
+    return (new_string.replace(":)","\N{slightly smiling face}").replace(":(","\N{slightly frowning face}"))
 main()
