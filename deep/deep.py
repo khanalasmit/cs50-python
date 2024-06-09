96f6279f6a1d46ed23d10c10b993598b814b68e3
khanalasmit <asmitpasa2063@gmail.com>
2024-06-09 05:40:40 +0000

    Sun, Jun 9, 2024, 11:25 AM +05:45

--- deep/deep.py
+++ deep/deep.py
@@ -1,6 +1,6 @@
 string=input("WHat is the Answer to the Great Question to Life, the Universe and Everything ? ")
-new=string.lower()
-if new=="42" or new=="forty two":
+new=string.lower().replace(" ","-")
+if new=="42" or new=="forty-two":
     print("YES")
 else:
     print("NO")
