cd12ee82f77dd385ac33097094a877d33529d2e6
khanalasmit <asmitpasa2063@gmail.com>
2024-06-13 14:23:25 +0000

    Thu, Jun 13, 2024, 8:08 PM +05:45

--- twttr/twttr.py
+++ twttr/twttr.py
@@ -1,11 +1,12 @@
-string=input("Input")
-string=list(string)
-i=0
-s=string.lower()
-while i<len(string):
-    if s=="a" or s[i]=="e" or s[i]=="i" or s[i]=="o" or s[i]=="u":
-        string.pop(i)
-    i+=1
-for x in string:
-    print(x,end="")
-
+def main():
+    string=list(input("Input: "))
+    li=["a","e","i","o","u","A","E","I","O","U"]
+    i=0
+    string=list(string)
+    while i<len(string):
+        if string[i] in li:
+            string.pop(i)
+        i+=1
+    for x in string:
+        print(x,end="")
+main()
