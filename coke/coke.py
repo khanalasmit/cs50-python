7847849cd3a76d3d69425248b9674b1c53a6654a
khanalasmit <asmitpasa2063@gmail.com>
2024-06-13 13:01:50 +0000

    Thu, Jun 13, 2024, 6:46 PM +05:45

---
+++ coke/coke.py
@@ -0,0 +1,11 @@
+amount=None
+sum=0
+while True:
+    amount=int(input("Insert coin: "))
+    if amount==25 or amount==10 or amount==5:
+        sum+=amount
+    if sum<50:
+        print(f"Amount due: ${50-sum}")
+    elif sum>=50:
+        print(f"Change owned: ${sum-50}")
+        break
