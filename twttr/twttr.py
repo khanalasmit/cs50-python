8c78b5b7740e3d2f6c54fc6ce3cf600fd055bd9d
khanalasmit <asmitpasa2063@gmail.com>
2024-06-13 13:39:46 +0000

    Thu, Jun 13, 2024, 7:24 PM +05:45

--- twttr/twttr.py
+++ twttr/twttr.py
@@ -2,8 +2,9 @@ string=input()
 string=list(string)
 i=0
 while i<len(string):
-    if string[i]=="a" or string[i]=="e" or string[i]=="i" or string[i]=="o" or string[i]=="u":
+    if string.lower()[i]=="a" or string.lower()[i]=="e" or string.lower()[i]=="i" or string.lower()[i]=="o" or string.lower()[i]=="u":
         string.pop(i)
     i+=1
 for x in string:
     print(x,end="")
+
