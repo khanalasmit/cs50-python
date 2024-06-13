479befacfd46ebbef73b1241294290da7db5cc4e
khanalasmit <asmitpasa2063@gmail.com>
2024-06-13 14:29:17 +0000

    Thu, Jun 13, 2024, 8:14 PM +05:45

--- twttr/twttr.py
+++ twttr/twttr.py
@@ -1,12 +1,14 @@
 def main():
     string=list(input("Input: "))
     li=["a","e","i","o","u","A","E","I","O","U"]
-    i=0
+    j=0
     string=list(string)
-    while i<len(string):
-        if string[i] in li:
-            string.pop(i)
-        i+=1
+    s=string
+    while j<len(s):
+        if string[j] in li:
+            string.pop(j)
+        else:
+            j+=1
     for x in string:
         print(x,end="")
 main()
