1ab40308f4491643d8485abf7e91e139bfd63df7
khanalasmit <asmitpasa2063@gmail.com>
2024-06-14 05:52:30 +0000

    Fri, Jun 14, 2024, 11:37 AM +05:45

--- plates/plates.py
+++ plates/plates.py
@@ -5,15 +5,24 @@ def main():
     else:
         print("Invalid")
 
+
 def is_valid(s):
     i=0
+    li=["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
     li2=["1","2","3","4","5","6","7","8","9","0"]
-    li=["1","2","3","4","5","6","7","8","9","0","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","z"]
-    while i<len(s):
-        if s[i] in li:
-            if s[1:len(s)] not in li2:
-                if 2<=len(s)<=6:
-                    return True
-        i+=1
+    if 2<=len(s)<=6:
+        while i<2:
+            if s[i] in li:
+                j=1
+                while j<(len(s)-1):
+                    if s[j] not in li2:
+                        k=0
+                        while k<len(s):
+                            if s[k] in li or s[k] in li2:
+                                return True
+                            k+=1
+                    j+=1
+            i+=1
+
 
 main()
