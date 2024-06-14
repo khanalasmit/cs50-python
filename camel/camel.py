0d780d8bf5ad3c6e6ef7dbe15357f2d3a9f6691b
khanalasmit <asmitpasa2063@gmail.com>
2024-06-14 07:55:13 +0000

    Fri, Jun 14, 2024, 1:40 PM +05:45

--- camel/camel.py
+++ camel/camel.py
@@ -1,23 +1,11 @@
-def main():
-    camelcase=input("camelCase: ")
-    index=snake(camelcase)
-    camelcase=list(camelcase)
-    i=0
-    print("snake_case:",end="")
-    while i<index:
-        print(camelcase[i],end="")
+camel=list(input("camelCase: "))
+i=0
+letters="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+print("snake_case:",end="")
+while i<len(camel):
+    if camel[i] not in letters:
+        print(camel[i],end="")
         i+=1
-    print("_",end="")
-    camelcase[index]=camelcase[index].lower()
-    while index<len(camelcase):
-        print(camelcase[index],end="")
-        index+=1
-def snake(camelcase):
-    camelcase=list(camelcase)
-    letters="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    i=0
-    while i<len(camelcase):
-        if camelcase[i] in letters:
-            return i
-        i+=1
-main()
+    elif camel[i] in letters:
+        print("_",end="")
+        camel[i]=camel[i].lower()
