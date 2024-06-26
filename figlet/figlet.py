902d9b669941f8a9c4bbd3a84f247c8553bc9c5d
khanalasmit <asmitpasa2063@gmail.com>
2024-06-26 12:54:34 +0000

    Wed, Jun 26, 2024, 6:39 PM +05:45

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -2,7 +2,10 @@ import pyfiglet
 import sys
 string=input("Input: ")
 try:
-    f=pyfiglet.figlet_format(string, font=sys.argv[2])
-    print(f)
+    if(sys.argv[1]=="f"):
+        f=pyfiglet.figlet_format(string, font=sys.argv[2])
+        print(f)
+    else:
+        sys.exit("Invalid usage")
 except pyfiglet.FontNotFound:
     sys.exit("Invlaid usage")
