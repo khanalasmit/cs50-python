b5252af947b59ed357d4e7cb876c4ba34d783a2b
khanalasmit <asmitpasa2063@gmail.com>
2024-06-26 13:46:04 +0000

    Wed, Jun 26, 2024, 7:31 PM +05:45

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -2,10 +2,11 @@ import pyfiglet
 import sys
 try:
     if len(sys.argv)>=2:
-        f=pyfiglet.figlet_format(font=sys.argv[2],input("Input: "),)
+        f=pyfiglet.Figlet(font=sys.argv[2])
         if sys.argv[1]=="-f":
+            string=input("Input: ")
             print("Output:")
-            print(f)
+            print(f.renderText(string))
         else:
             sys.exit("Invalid usage")
     else:
