c6f89aa312d9bb2e712841b4dfabe13090221ae0
khanalasmit <asmitpasa2063@gmail.com>
2024-06-26 12:56:35 +0000

    Wed, Jun 26, 2024, 6:41 PM +05:45

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -2,7 +2,7 @@ import pyfiglet
 import sys
 string=input("Input: ")
 try:
-    if(sys.argv[1]=="f"):
+    if(sys.argv[1]=="-f"):
         f=pyfiglet.figlet_format(string, font=sys.argv[2])
         print(f)
     else:
