b21fb97e27f8071f586140fb31267c68271637db
khanalasmit <asmitpasa2063@gmail.com>
2024-07-01 10:23:44 +0000

    Mon, Jul 1, 2024, 4:08 PM +05:45

---
+++ bitcoin/bitcoin.py
@@ -0,0 +1,18 @@
+import requests
+import sys
+try:
+    r = requests.get("https://api.coindesk.com/v1/bpi/currentprice.json")
+    p = r.json()
+    a=p["bpi"]["USD"]["rate_float"]
+    try:
+        b=float(sys.argv[1])
+    except IndexError:
+        print("Missing command-line argument")
+    except ValueError:
+        print("Command-line argument is not a number")
+        sys.exit()
+except requests.RequestException:
+    print("Missing command-line argument")
+    sys.exit()
+amount=a*b
+print(f"{amount:,.4f}")
