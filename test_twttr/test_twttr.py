cf15f945d8c2802049adbd54ee848c4129240395
khanalasmit <asmitpasa2063@gmail.com>
2024-07-04 10:34:04 +0000

    Thu, Jul 4, 2024, 4:19 PM +05:45

--- test_twttr/test_twttr.py
+++ test_twttr/test_twttr.py
@@ -1,10 +1,5 @@
 from twttr import shorten
 
 def test_shorten():
-    assert shorten("cat") == list("ct")
-    assert shorten("hello") == list("hll")
-    assert shorten("aeiou") == []
-    assert shorten("AEIOU") == []
-    assert shorten("Python") == list("Pythn")
-
+    assert shorten("cat") == "ct"
 
