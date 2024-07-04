4b19a51b81dbe18a4077c40d268c6cb21d2f081e
khanalasmit <asmitpasa2063@gmail.com>
2024-07-04 10:46:10 +0000

    Thu, Jul 4, 2024, 4:31 PM +05:45

--- test_twttr/test_twttr.py
+++ test_twttr/test_twttr.py
@@ -3,3 +3,5 @@ from twttr import shorten
 def test_shorten():
     assert shorten("cat") == "ct"
     assert shorten("CAT") =="CT"
+    assert shorten("CAT1")=="CT1"
+    assert shorten("CA!1")=="C!1"
