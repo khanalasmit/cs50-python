cd740ac87470d2693499d6836bd63bc7e573f2c4
khanalasmit <asmitpasa2063@gmail.com>
2024-07-04 10:09:35 +0000

    Thu, Jul 4, 2024, 3:54 PM +05:45

--- twttr/twttr.py
+++ twttr/twttr.py
@@ -1,16 +1,15 @@
 def main():
-    string=list(input("Input: "))
-    
+    word=list(input("Input: "))
+    string=shorten(word)
 def shorten(word):
     li=["a","e","i","o","u","A","E","I","O","U"]
     j=0
-    string=list(string)
-    s=string
+    word=list(word)
+    s=word
     while j<len(s):
         if string[j] in li:
             string.pop(j)
         else:
             j+=1
-    for x in string:
-        print(x,end="")
+    return word
 main()
