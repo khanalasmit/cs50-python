0d35162772699f8ce38ef0aaa75b5760eb9ab4f2
khanalasmit <asmitpasa2063@gmail.com>
2024-07-11 15:54:03 +0000

    Thu, Jul 11, 2024, 9:39 PM +05:45

---
+++ lines/lines.py
@@ -0,0 +1,27 @@
+import sys
+
+def main():
+    if len(sys.argv) > 2:
+        print("Too many command-line arguments")
+        return
+    if len(sys.argv)<2:
+        print("Too few command-line arguments")
+        return
+    first,second=sys.argv[1].split(".")
+    if second!="py":
+        print("Not a Python file")
+    i = 0
+    try:
+        with open(sys.argv[1], "r") as file:
+            while True:
+                line = file.readline()
+                if not line:
+                    break
+                if line.strip() and not line.lstrip().startswith('#'):
+                    i += 1
+            print(i)
+    except FileNotFoundError:
+        print("File does not exists")
+
+if __name__ == "__main__":
+    main()
