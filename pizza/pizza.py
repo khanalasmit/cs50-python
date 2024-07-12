330b3cf28842fed6677b88a342e1f03d8dc7ea82
khanalasmit <asmitpasa2063@gmail.com>
2024-07-12 07:18:34 +0000

    Fri, Jul 12, 2024, 1:03 PM +05:45

---
+++ pizza/pizza.py
@@ -0,0 +1,22 @@
+import sys
+import tabulate
+import csv
+pizza=[]
+if len(sys.argv) > 2:
+    sys.exit("Too many command-line arguments")
+if len(sys.argv)<2:
+    sys.exit("Too few command-line arguments")
+if ".csv" not in sys.argv[1]:
+    sys.exit("Not a CSV file")
+try:
+    with open(sys.argv[1],"r") as file:
+        reader=csv.reader(file)
+        for one,two,three in reader:
+            break
+        for first,second,third in reader:
+            pizza.append({one:first,two:second,three:third})
+    pizza.pop(0)
+    table=tabulate.tabulate(pizza, headers="keys", tablefmt="fancy_grid")
+except FileNotFoundError:
+    sys.exit("File does not exists")
+print(table)
