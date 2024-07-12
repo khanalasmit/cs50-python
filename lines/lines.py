9be41089078c4f4bb80ba0dd1501b4cad8f40fe5
khanalasmit <asmitpasa2063@gmail.com>
2024-07-12 06:07:53 +0000

    Fri, Jul 12, 2024, 11:52 AM +05:45

--- lines/lines.py
+++ lines/lines.py
@@ -6,10 +6,10 @@ def main():
     if len(sys.argv)<2:
         sys.exit("Too few command-line arguments")
     if "." not in sys.argv[1]:
-        sys.exit("Not a python file")
+        sys.exit("Not a Python file")
     first,second=sys.argv[1].split(".")
     if second!="py":
-        print("Not a Python file")
+        sys.exit("Not a Python file")
     i = 0
     try:
         with open(sys.argv[1], "r") as file:
