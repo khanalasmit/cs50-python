a1a8e3cbc59b067d0ddcd02527710b71146af66e
khanalasmit <asmitpasa2063@gmail.com>
2024-07-15 16:43:42 +0000

    Mon, Jul 15, 2024, 10:28 PM +05:45

--- scourgify/scourgify.py
+++ scourgify/scourgify.py
@@ -9,19 +9,21 @@ if ".csv" not in sys.argv[1]:
     sys.exit("Not a CSV file")
 students=[]
 try:
-    with open(sys.argv[1],"r") as file:
-        reader=csv.DictReader(file)
+    with open(sys.argv[1], "r") as file:
+        reader = csv.DictReader(file)
         for row in reader:
             students.append(row)
 except FileNotFoundError:
     sys.exit(f"Could not read {sys.argv[1]}")
+
+# Try to write to the output CSV file
 try:
-    with open(sys.argv[2],"w") as fp:
-        writer=csv.DictWriter(fp,fieldnames=['first','last','house'])
+    with open(sys.argv[2], "w", newline='') as fp:
+        writer = csv.DictWriter(fp, fieldnames=['first', 'last', 'house'])
+        writer.writeheader()
         for row in students:
-            one,two=row["name"].split(",")
-            pupil=({"first":one,"last":two,"house":row["house"]})
-            writer.writeheader()
+            last, first = row["name"].split(",")
+            pupil = {"first": first.strip(), "last": last.strip(), "house": row["house"]}
             writer.writerow(pupil)
-except Exception:
-    sys.exit(f"Could not read {sys.argv[2]}")
+except Exception as e:
+    sys.exit(f"Could not write to {sys.argv[2]}")
