bb455c67212d928299223db56f76e0ea025353c3
khanalasmit <asmitpasa2063@gmail.com>
2024-07-23 14:39:03 +0000

    Tue, Jul 23, 2024, 8:24 PM +05:45

--- shirt/practice.py
+++ shirt/practice.py
@@ -1,8 +1,8 @@
 from PIL import Image
 import sys
-im=Image.open("before1.jpg")
-im_n=im.resize((600,600))
-im_n.save("nbefore.jpg")
-photo.paste(")
+im=Image.open("shirt.png")
+base=Image.open("nbefore.jpg")
+base.paste(im)
+base.save("new.jpg")
 
 
