1824ae0e4823dc70a13b6ce46e5198b3bde3f0dc
khanalasmit <asmitpasa2063@gmail.com>
2024-08-12 12:28:35 +0000

    Mon, Aug 12, 2024, 6:13 PM +05:45

--- shirt/demo.py
+++ shirt/demo.py
@@ -1,3 +1,7 @@
-from pip import Image
+from PIL import Image
 img=Image.open("shirt.png")
-print(img.size())
+print(img.size)
+img1=Image.open("before1.jpg")
+print(img1.size)
+new=img1.resize(1200,600)
+new.save("nbefore1.png")
