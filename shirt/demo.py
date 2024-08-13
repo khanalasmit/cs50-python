91f791bc47fdfc6ded38047f76fc979b9085d51c
khanalasmit <asmitpasa2063@gmail.com>
2024-08-13 03:05:51 +0000

    Tue, Aug 13, 2024, 8:50 AM +05:45

--- shirt/demo.py
+++ shirt/demo.py
@@ -2,10 +2,10 @@ from PIL import Image
 img=Image.open("shirt.png")
 img1=Image.open("before1.jpg")
 new=img1.resize(img.size)
-print(new.size)
-new.save("nbefore1.png")
 if img.mode!=new.mode:
     img=img.convert(new.mode)
+new=Image.new("RGBA")
+
 new.paste(new,(0,0),img)
 new.save("final.png")
 
